e50b42e4db5eb83443e38a788927c20820512fd9
yuhei-harada-dev <yuhei.harada.dev@gmail.com>
2025-11-01 19:53:11 +0900

    feat: FastAPIの初期アプリケーションを追加

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+# FastAPIのインスタンスを作成
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    """
+    ルートURL ( / ) へのGETリクエストに応答する
+    """
+    return {"message": "Hello World from FastAPI"}
